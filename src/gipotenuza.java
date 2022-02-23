40cde936cd06b14c1d352ff1a722119dfe3a2206
AlexLevanchenko <alex.levanchenko@gmail.com>
2022-02-24 00:42:43 +0200

    Initial commit

---
+++ src/gipotenuza.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class gipotenuza {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Для расчета гипотенузы, введите значение катетов.");
+        double a, b, c;
+        System.out.print("\nВведите значение a: ");
+        a = input.nextDouble();
+        System.out.print("Введите значение b: ");
+        b = input.nextDouble();
+        if (a <= 0 || b <= 0) {
+            System.out.print("Значение не может быть меньше или равно нулю");
+        } else {
+            c = Math.sqrt(a * a + b * b);
+            System.out.print("Гипотенуза: " + c);
+        }
+    }
+}
